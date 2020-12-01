3dbd9235adbb48e8bf996ce0e43a403d4b3d5929
Vanessa de Moraes Jansen <vanessamjansen@gmail.com>
2020-12-01 09:59:48 -0300

    Ajustes

--- jansensfilms/src/routes/index.js
+++ jansensfilms/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get("/", function (req, res) {
     res.status(200).send({
-        title: "Reprograma Semana 11 - Projeto Revisao",
+        title: "Minha API Rest Nodejs",
         version: "1.0.0"
     })
 })
